639c84c0ab031c29f0991d722ed9fb71395dd844
seungwoo Kim <kyh196201@gmail.com>
2022-01-10 23:20:47 +0900

    기본 레이아웃 설정

---
+++ src/layouts/app-layout.vue
@@ -0,0 +1,24 @@
+<template>
+	<component :is="layout">
+		<slot></slot>
+	</component>
+</template>
+
+<script>
+// Layouts
+import defaultLayout from './default-layout.vue';
+
+export default {
+	components: {
+		defaultLayout,
+	},
+
+	data() {
+		return {
+			layout: 'defaultLayout',
+		};
+	},
+};
+</script>
+
+<style></style>
---
+++ src/layouts/default-layout.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<header class="header">헤더</header>
+		<slot></slot>
+		<div class="dock-bar">독바 영역</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
