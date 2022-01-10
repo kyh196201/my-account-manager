ca57931e77e705ffb0e994ecf9aa9efc8542b8e5
seungwoo Kim <kyh196201@gmail.com>
2022-01-11 01:48:26 +0900

    vue-composition api 설치 및 적용

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,9 +1,25 @@
 <template>
-	<div>Home Page</div>
+	<div>
+		Home Page
+
+		<span>{{ message }}</span>
+	</div>
 </template>
 
 <script>
+import { ref } from '@vue/composition-api';
+
 export default {
 	name: 'HomePage',
+
+	setup(props) {
+		console.log(props);
+
+		const message = ref('hello');
+
+		return {
+			message,
+		};
+	},
 };
 </script>
