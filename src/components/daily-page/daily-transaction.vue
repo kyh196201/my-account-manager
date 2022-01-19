f47d6600ef76c2a2689e4925587860ae11add9f6
seungwoo Kim <kyh196201@gmail.com>
2022-01-20 07:52:11 +0900

    소비/지출 컴포넌트 샘플 데이터 바인딩

--- src/components/daily-page/daily-transaction.vue
+++ src/components/daily-page/daily-transaction.vue
@@ -2,8 +2,12 @@
 	<div class="daily-transaction">
 		<div class="daily-transaction__summary">
 			<span class="daily-transaction__date"> 2022.01.15 토요일 </span>
-			<strong class="daily-transaction__const outcome"> 0원 </strong>
-			<strong class="daily-transaction__const income"> 18040원 </strong>
+			<strong class="daily-transaction__const income">
+				{{ totalIncome }}원
+			</strong>
+			<strong class="daily-transaction__const outcome">
+				{{ totalOutcome }}원
+			</strong>
 		</div>
 		<div class="daily-transaction__detail">
 			<ul class="daily-transaction__list">
@@ -12,18 +16,7 @@
 						<span class="transaction__title">식비</span>
 						<div class="transaction__detail">
 							<span class="transaction__method">현금</span>
-							<span class="transaction__timestamp">
-								오후 11:14
-							</span>
-						</div>
-						<strong class="transaction__cost">18040원</strong>
-					</div>
-				</li>
-				<li>
-					<div class="transaction outcome">
-						<span class="transaction__title">식비</span>
-						<div class="transaction__detail">
-							<span class="transaction__method">현금</span>
+							<span class="transaction__desc">붕어빵 사먹음</span>
 							<span class="transaction__timestamp">
 								오후 11:14
 							</span>
@@ -37,8 +30,75 @@
 </template>
 
 <script>
+import { ref, computed } from '@vue/composition-api';
+
+const TRANSACTION_TYPE = {
+	INCOME: 'income', // 수입
+	OUTCOME: 'outcome', // 지출
+};
+
+const mockData = {
+	date: '2022-01-15',
+	transactionList: [
+		{
+			category: '식비',
+			method: '현금',
+			description: '붕어빵',
+			cost: '8050',
+			type: TRANSACTION_TYPE.OUTCOME,
+		},
+		{
+			category: '식비',
+			method: '카드',
+			description: '점심 식비',
+			cost: '10000',
+			type: TRANSACTION_TYPE.OUTCOME,
+		},
+	],
+};
+
 export default {
 	name: 'DailyTransaction',
+
+	setup() {
+		const data = ref(mockData);
+
+		// 총 지출 금액
+		const totalOutcome = computed(() => {
+			const transactionList = data.value.transactionList;
+			let total = 0;
+
+			for (let i = 0; i < transactionList.length; i++) {
+				const transaction = transactionList[i];
+				if (transaction.type === TRANSACTION_TYPE.OUTCOME) {
+					total += transaction.cost;
+				}
+			}
+
+			return total;
+		});
+
+		// 총 수입 금액
+		const totalIncome = computed(() => {
+			const transactionList = data.value.transactionList;
+			let total = 0;
+
+			for (let i = 0; i < transactionList.length; i++) {
+				const transaction = transactionList[i];
+				if (transaction.type === TRANSACTION_TYPE.INCOME) {
+					total += transaction.cost;
+				}
+			}
+
+			return total;
+		});
+
+		return {
+			data,
+			totalOutcome,
+			totalIncome,
+		};
+	},
 };
 </script>
 
@@ -95,6 +155,10 @@ export default {
 				text-align: right;
 				font-size: 1.7rem;
 				font-weight: 500;
+			}
+
+			&__desc {
+				font-weight: bolder;
 			}
 
 			&.income {
