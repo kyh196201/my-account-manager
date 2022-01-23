8be1bce75169d28874365eab4c0202cf06a6aafc
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2022-01-23 16:23:25 +0900

    base-input 컴포넌트 구현

---
+++ src/components/common/base-input.vue
@@ -0,0 +1,28 @@
+<template>
+	<input
+		class="base-input"
+		type="text"
+		:value="value"
+		@input="$emit('input', $event.target.value)"
+	/>
+</template>
+
+<script>
+export default {
+	name: 'BaseInput',
+
+	props: {
+		value: {
+			type: String,
+			default: '',
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.base-input {
+	width: 100%;
+}
+</style>
