2ebcdffdc1f091b977e5427441222d1fdb7b7204
seungwoo Kim <kyh196201@gmail.com>
2022-01-24 01:14:47 +0900

    유틸함수 추가

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,5 +1,18 @@
+// 금액에 콤마 찍기
 function numberWithCommas(x = 0) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-export { numberWithCommas };
+// 숫자를 제외한 모든 문자 제거
+// https://bobbyhadz.com/blog/javascript-remove-all-characters-except-numbres
+function filterOnlyNumbers(string = '') {
+	return string.replace(/[^\d]/g, '');
+}
+
+// 숫자로만 이루어진 문자열인지 체크
+function isOnlyNumberString(string = '') {
+	const reg = /^\d+$/;
+	return reg.test(string);
+}
+
+export { numberWithCommas, filterOnlyNumbers, isOnlyNumberString };
