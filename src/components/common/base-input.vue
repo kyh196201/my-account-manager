ef90e07971f3157f2e5b7b897136195347da58db
seungwoo Kim <kyh196201@gmail.com>
2022-01-24 01:14:35 +0900

    base-input 컴포넌트 부모에서 지정한 이벤트 수신할 수 있도록 수정

--- src/components/common/base-input.vue
+++ src/components/common/base-input.vue
@@ -3,7 +3,7 @@
 		class="base-input"
 		type="text"
 		:value="value"
-		@input="$emit('input', $event.target.value)"
+		v-on="inputListeners"
 	/>
 </template>
 
@@ -16,6 +16,20 @@ export default {
 			type: String,
 			default: '',
 			required: true,
+		},
+	},
+
+	computed: {
+		// https://kr.vuejs.org/v2/guide/components-custom-events.html#%EB%84%A4%EC%9D%B4%ED%8B%B0%EB%B8%8C-%EC%9D%B4%EB%B2%A4%ED%8A%B8%EB%A5%BC-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8%EC%97%90-%EB%B0%94%EC%9D%B8%EB%94%A9-%ED%95%98%EA%B8%B0
+		inputListeners() {
+			const vm = this;
+
+			return Object.assign({}, this.$listeners, {
+				// $listeners안에 있는 input을 오버라이드함
+				input(event) {
+					vm.$emit('input', event.target.value);
+				},
+			});
 		},
 	},
 };
